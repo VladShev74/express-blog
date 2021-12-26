d1f5375330bbf5bc80555784a2f524f9182ab6ae
VladShev74 <vladadshev@gmail.com>
2021-12-26 10:54:56 +0200

    Fixed auth validator

--- src/validationSchemas/authValidator.js
+++ src/validationSchemas/authValidator.js
@@ -7,10 +7,10 @@ exports.authRegister = yup.object().shape({
   lastName: yup.string().min(2).required(),
   location: yup.string(),
   avatarUrl: yup.string().url(),
-  githubUrl: yup.string().url(),
-  description: yup.string().min(3),
-  work: yup.string().min(3),
-  hobby: yup.string().min(3),
+  githubUrl: yup.string().url().optional(),
+  description: yup.string().optional(),
+  work: yup.string().optional(),
+  hobby: yup.string().optional(),
   birthDate: yup.date(),
 });
 
